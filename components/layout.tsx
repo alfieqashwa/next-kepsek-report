8d382aa1068b84f2aa6445aec387f44328c6adad
alfieqashwa <alfieqashwa@gmail.com>
2022-02-10 13:51:52 +0700

    fix: modify main layout's breakpoints

--- components/layout.tsx
+++ components/layout.tsx
@@ -17,82 +17,74 @@ export default function Layout({ children }: Props): JSX.Element {
         <title>Kepsek Report</title>
       </Head>
 
-      <header className="flex items-center justify-around pt-10 mx-8 lg:pt-16 lg:mx-28">
-        <span className="absolute top-0 right-0 hidden w-24 md:block">
-          <Image
-            src="/images/banner-jaringan-idn-white.svg"
-            alt="Logo of Jaringan IDN"
-            width={195}
-            height={111}
-          />
-        </span>
-
-        {/* School Logo */}
-        <section className="flex items-center justify-start space-x-2">
-          <span className="w-14 h-14">
-            <Image src="/images/image-3.svg" width={100} height={100} alt="logo-school" />
+      <div className="container max-w-3xl mx-auto">
+        <header className="flex items-center justify-around pt-10 lg:pt-16">
+          <span className="absolute top-0 right-0 hidden w-24 md:block">
+            <Image
+              src="/images/banner-jaringan-idn-white.svg"
+              alt="Logo of Jaringan IDN"
+              width={195}
+              height={111}
+            />
           </span>
-          <div className="text-[#F1FFF8] flex flex-col justify-center items-start">
-            <h1 className="font-semibold leading-4 lg:font-bold text-md lg:text-3xl">Laporan Kepala Sekolah</h1>
-            <h3 className="text-sm lg:text-2xl">SMK Bakti Karya Parigi</h3>
-          </div>
-        </section>
 
-        {/* Trophy */}
-        <section className="bg-[#FFFFF4] font-primary rounded-lg px-3 lg:px-4 py-1 flex justify-center items-center space-x-3 lg:space-x-6">
-          <div className="h-10 w-9">
-            <Image src="/images/layer-1.svg" alt="trophy" width={51.25} height={60.85} />
-          </div>
-          <div>
-            <h4 className="text-xs lg:text-base text-[#F2994A]">Points</h4>
-            <p className="font-bold leading-4 text-md lg:text-3xl text-meteorite">120.000</p>
-          </div>
-          <div className="w-4 h-4">
-            <Image src="/images/icon_btn-next_yellow.svg" alt="trophy" width={30} height={30} />
-          </div>
-        </section>
-        {/* 
-        <nav className="fixed flex flex-col left-[450px] z-90 inset-y-64">
-          <a href="#home" className="w-60 -ml-60 p-4 rounded-l-2xl bg-white drop-shadow-2xl shadow-2xl text-[#008169]">
-            Home
-          </a>
-          <a href="#data-digital" className="p-4 active:bg-white rounded-l-2xl shadow-inner bg-[#FAFAFA] text-[#A4A4A4] w-60 -ml-60">
-            Data Digital
-          </a>
-          <a href="#keuangan-digital" className="p-4 rounded-l-2xl shadow-inner bg-[#FAFAFA] text-[#A4A4A4] w-60 -ml-60">
-            Keuangan Digital
-          </a>
-          <a href="#absensi-digital" className="p-4 rounded-l-2xl shadow-inner bg-[#FAFAFA] text-[#A4A4A4] w-60 -ml-60">
-            Absensi Digital
-          </a>
-          <a href="#toko-pelajar" className="p-4 rounded-l-2xl shadow-inner bg-[#FAFAFA] text-[#A4A4A4] w-60 -ml-60">
-            Toko Pelajar
-          </a>
-          <a href="#sign-out" className="p-4 rounded-l-2xl shadow-inner bg-[#FAFAFA] text-[#A4A4A4] w-60 -ml-60">
-            Sign Out
-          </a>
-        </nav> */}
-      </header>
-
-      <section className="relative flex justify-end mx-24 mt-6">
-        <main className="max-w-md p-6 bg-white shadow-xl drop-shadow-xl rounded-2xl">{children}</main>
-      </section>
-
-      {/* FOOTER */}
-      <footer className="ml-56 mr-24 my-7 font-primary">
-        <p className="text-xs text-black">Terdapat pertanyaan atau kendala ?</p>
-        <section className="flex items-center justify-between mt-0.5">
-          <button className="flex space-x-2 justify-center items-center bg-[#008169] rounded-full pl-2 pr-4 py-1 hover:opacity-90 transition-all duration-150 ease-in-out shadow-md shrink-0">
-            <span className="w-4 h-4">
-              <Image src="/images/img_whatsapp.svg" width={23} height={23} alt="Contact logo" />
+          {/* School Logo */}
+          <section className="flex items-center justify-start space-x-2">
+            <span className="w-14 h-14">
+              <Image src="/images/image-3.svg" width={100} height={100} alt="logo-school" />
             </span>
-            <p className="text-xs text-white font-md">Hubungi tim CS Kami</p>
-          </button>
-          <span className="w-32">
-            <Image src="/images/powered-by-idn.svg" width={208} height={36.06} alt="Contact logo" />
-          </span>
+            <div className="text-[#F1FFF8] flex flex-col justify-center items-start">
+              <h1 className="font-semibold leading-4 lg:font-bold text-md lg:text-3xl">Laporan Kepala Sekolah</h1>
+              <h3 className="text-sm lg:text-2xl">SMK Bakti Karya Parigi</h3>
+            </div>
+          </section>
+
+          {/* Trophy */}
+          <section className="bg-[#FFFFF4] font-primary rounded-lg px-3 lg:px-4 py-1 flex justify-center items-center space-x-3 lg:space-x-6">
+            <div className="h-10 w-9">
+              <Image src="/images/layer-1.svg" alt="trophy" width={51.25} height={60.85} />
+            </div>
+            <div>
+              <h4 className="text-xs lg:text-base text-[#F2994A]">Points</h4>
+              <p className="font-bold leading-4 text-md lg:text-3xl text-meteorite">120.000</p>
+            </div>
+            <div className="w-4 h-4">
+              <Image src="/images/icon_btn-next_yellow.svg" alt="trophy" width={30} height={30} />
+            </div>
+          </section>
+
+          <nav className="fixed flex flex-col left-[450px] z-90 inset-y-64">
+            <a href="#home" className="text-xs w-60 -ml-80 p-2 rounded-l-lg bg-white drop-shadow-2xl shadow-2xl text-[#008169]">
+              Home
+            </a>
+            <a href="#data-digital" className="p-2 text-xs active:bg-white rounded-l-lg shadow-inner bg-[#FAFAFA] text-[#A4A4A4] w-60 -ml-80">
+              Data Digital
+            </a>
+          </nav>
+        </header>
+
+        <section className="mx-10 mt-6 sm:mx-auto sm:max-w-lg md:max-w-xl lg:mx-8 lg:max-w-3xl bg-rose-50">
+          <div className="flex justify-end">
+            <main className="w-2/3 p-6 bg-white shadow-xl drop-shadow-xl rounded-2xl">{children}</main>
+          </div>
         </section>
-      </footer>
+
+        {/* FOOTER */}
+        <footer className="ml-56 mr-24 my-7 font-primary">
+          <p className="text-xs text-black">Terdapat pertanyaan atau kendala ?</p>
+          <section className="flex items-center justify-between mt-0.5">
+            <button className="flex space-x-2 justify-center items-center bg-[#008169] rounded-full pl-2 pr-4 py-1 hover:opacity-90 transition-all duration-150 ease-in-out shadow-md shrink-0">
+              <span className="w-4 h-4">
+                <Image src="/images/img_whatsapp.svg" width={23} height={23} alt="Contact logo" />
+              </span>
+              <p className="text-xs text-white font-md">Hubungi tim CS Kami</p>
+            </button>
+            <span className="w-32">
+              <Image src="/images/powered-by-idn.svg" width={208} height={36.06} alt="Contact logo" />
+            </span>
+          </section>
+        </footer>
+      </div>
     </div>
   )
 }
