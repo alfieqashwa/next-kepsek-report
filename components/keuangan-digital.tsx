38aa7761ed864490dde72b6a1c822f87cb6a8132
alfieqashwa <alfieqashwa@gmail.com>
2022-02-16 03:34:23 +0700

    fix: all-steps of keuangan-digital

--- components/keuangan-digital.tsx
+++ components/keuangan-digital.tsx
@@ -27,30 +27,11 @@ export const KeuanganDigital = () => (
                 <span>tahap:</span><span className="text-green-600">mulai</span>
             </h2>
             {/* Tahap-Tahap / Steps */}
-            <div className="my-5 py-8 -ml-[72px] -mr-6 md:-ml-20 md:-mr-8 px-24 bg-[#F9FFFF]">
-                <section className="flex items-start justify-around">
-                    <div className="flex flex-col items-center">
-                        <button className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">1</button>
-                        <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
-                        <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">training</h4>
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <button className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">2</button>
-                        <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
-                        <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">data upload</h4>
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <button className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">3</button>
-                        <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
-                        <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">sosialisasi</h4>
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <button className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">4</button>
-                        <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
-                        <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">aktif</h4>
-                    </div>
-                </section>
-            </div>
+            <KeuanganSteps0 />
+            <KeuanganSteps1 />
+            <KeuanganSteps2 />
+            <KeuanganSteps3 />
+            <KeuanganSteps4 />
             <div className="pr-10 leading-5">
                 <p>Ayo, segera mulai program Keuangan Digital, mulai dari jadwalkan staff keuangan / bendahara Anda untuk mengikuti training bersama jaringan IDN. Persiapkan staff keuangan / bendahara Anda dalam perkembangan teknologi digital.</p>
             </div>
@@ -72,3 +53,167 @@ export const KeuanganDigital = () => (
         </section>
     </div>
 )
+function KeuanganSteps0() {
+    return (
+        <div className="my-5 py-8 -ml-[72px] -mr-6 md:-ml-20 md:-mr-8 px-24 bg-[#F9FFFF]">
+            <section className="flex items-start justify-around">
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">1</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">training</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">2</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">data upload</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">3</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">sosialisasi</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">4</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">aktif</h4>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+function KeuanganSteps1() {
+    return (
+        <div className="my-5 py-8 -ml-[72px] -mr-6 md:-ml-20 md:-mr-8 px-24 bg-[#F9FFFF]">
+            <section className="flex items-start justify-around">
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#42AF7A] shadow-[#42AF7A]/50 text-white whitespace-nowrap font-bold text-xs shrink-0 rounded-full py-1.5 px-3 shadow-xl">Langkah 1</span>
+                    <span className="relative bg-[#169859] shadow-lg shadow-[#169859]/50 text-center shrink-0 flex justify-center items-center w-14 h-14 rounded-lg mt-8 -rotate-45">
+                        <Image src="/images/tilda-training.svg" width={30} height={30} alt="tilda-training" className="text-white rotate-45" />
+                    </span>
+                    <h4 className="text-sm font-bold text-[#2A2A2A] capitalize mt-6">training</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">2</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">data upload</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">3</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">sosialisasi</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">4</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">aktif</h4>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+function KeuanganSteps2() {
+    return (
+        <div className="my-5 py-8 -ml-[72px] -mr-6 md:-ml-20 md:-mr-8 px-24 bg-[#F9FFFF]">
+            <section className="flex items-start justify-around">
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">1</span>
+                    <span className="bg-[#9ADEBD] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9">
+                        <Image src="/images/check-btn.svg" width={28} height={28} alt="check" className="rotate-45" />
+                    </span>
+                    <h4 className="text-xs font-bold text-[#169859] capitalize mt-5">training</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#42AF7A] shadow-[#42AF7A]/50 text-white whitespace-nowrap font-bold text-xs shrink-0 rounded-full py-1.5 px-3 shadow-xl">Langkah 2</span>
+                    <span className="relative bg-[#169859] shadow-lg shadow-[#169859]/50 text-center shrink-0 flex justify-center items-center w-14 h-14 rounded-lg mt-8 -rotate-45">
+                        <Image src="/images/tilda-data-upload.svg" width={30} height={30} alt="tilda-data-upload" className="text-white rotate-45" />
+                    </span>
+                    <h4 className="text-sm font-bold text-[#2A2A2A] capitalize mt-6">data upload</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">3</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">sosialisasi</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">4</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">aktif</h4>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+function KeuanganSteps3() {
+    return (
+        <div className="my-5 py-8 -ml-[72px] -mr-6 md:-ml-20 md:-mr-8 px-24 bg-[#F9FFFF]">
+            <section className="flex items-start justify-around">
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">1</span>
+                    <span className="bg-[#9ADEBD] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9">
+                        <Image src="/images/check-btn.svg" width={28} height={28} alt="check" className="rotate-45" />
+                    </span>
+                    <h4 className="text-xs font-bold text-[#169859] capitalize mt-5">training</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">2</span>
+                    <span className="bg-[#9ADEBD] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9">
+                        <Image src="/images/check-btn.svg" width={28} height={28} alt="check" className="rotate-45" />
+                    </span>
+                    <h4 className="text-xs font-bold text-[#169859] capitalize mt-5">data upload</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#42AF7A] shadow-[#42AF7A]/50 text-white whitespace-nowrap font-bold text-xs shrink-0 rounded-full py-1.5 px-3 shadow-xl">Langkah 3</span>
+                    <span className="relative bg-[#169859] shadow-lg shadow-[#169859]/50 text-center shrink-0 flex justify-center items-center w-14 h-14 rounded-lg mt-8 -rotate-45">
+                        <Image src="/images/tilda-sosialisasi.svg" width={30} height={30} alt="tilda-sosialisasi" className="text-white rotate-45" />
+                    </span>
+                    <h4 className="text-sm font-bold text-[#2A2A2A] capitalize mt-6">sosialisasi</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 rounded-full font-bold text-sm text-[#2A2A2A]">4</span>
+                    <span className="bg-[#D7D7D7] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9"></span>
+                    <h4 className="text-xs font-bold text-[#A4A4A4] capitalize mt-5">aktif</h4>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+function KeuanganSteps4() {
+    return (
+        <div className="my-5 py-8 -ml-[72px] -mr-6 md:-ml-20 md:-mr-8 px-24 bg-[#F9FFFF]">
+            <section className="flex items-start justify-around">
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">1</span>
+                    <span className="bg-[#9ADEBD] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9">
+                        <Image src="/images/check-btn.svg" width={28} height={28} alt="check" className="rotate-45" />
+                    </span>
+                    <h4 className="text-xs font-bold text-[#169859] capitalize mt-5">training</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">2</span>
+                    <span className="bg-[#9ADEBD] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9">
+                        <Image src="/images/check-btn.svg" width={28} height={28} alt="check" className="rotate-45" />
+                    </span>
+                    <h4 className="text-xs font-bold text-[#169859] capitalize mt-5">data upload</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#D5EEE2] py-1.5 px-3 font-bold rounded-full text-sm text-[#2A2A2A]">3</span>
+                    <span className="bg-[#9ADEBD] shrink-0 text-center flex justify-center items-center w-10 h-10 rounded-lg -rotate-45 mt-9">
+                        <Image src="/images/check-btn.svg" width={28} height={28} alt="check" className="rotate-45" />
+                    </span>
+                    <h4 className="text-xs font-bold text-[#169859] capitalize mt-5">sosialisasi</h4>
+                </div>
+                <div className="flex flex-col items-center">
+                    <span className="bg-[#42AF7A] shadow-[#42AF7A]/50 text-white whitespace-nowrap font-bold text-xs shrink-0 rounded-full py-1.5 px-3 shadow-xl">Langkah 4</span>
+                    <span className="relative bg-[#2D9CDB] shadow-lg shadow-[#2D9CDB]/50 text-center shrink-0 flex justify-center items-center w-14 h-14 rounded-lg mt-8 -rotate-45">
+                        <Image src="/images/tilda-aktif.svg" width={30} height={30} alt="tilda-aktif" className="text-white rotate-45" />
+                    </span>
+                    <h4 className="text-sm font-bold text-[#2A2A2A] capitalize mt-6">aktif</h4>
+                </div>
+            </section>
+        </div>
+    )
+}
